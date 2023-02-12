4ccd2f1c5cc8257f0513f5a5478a5355ca7973bd
Lejay <lesego777@gmail.com>
2023-02-12 15:27:52 +0200

    update

---
+++ lib/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+
+const Color primary = Color.fromARGB(255, 0, 247, 255);
+const Color secondary = Color(0xFF2B2B2B);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
